a68b23cec2af77509635707af8c9f4c40abf7f8b
UmaidBinZubair <umaidbinzubair@gmail.com>
2024-04-18 14:42:49 +0200

    added views with render to access blog pages

---
+++ django_project/blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import post
+
+def home(request):
+    context = {'posts':post.objects.all()}
+    return render(request,'blog/home.html' , context)
+
+def about(request):
+    return render(request, 'blog/about.html',{'title':'About'})
+
+
